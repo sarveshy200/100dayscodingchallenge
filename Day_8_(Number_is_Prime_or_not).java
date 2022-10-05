0e02540ced5ef4dff192d65dbabad36e7c2aeaeb
SARVESH KUMAR YADAV <105293807+sarveshy200@users.noreply.github.com>
2022-10-05 23:59:18 +0530

    Update Day_8_(Number_is_Prime_or_not).java

--- Day_8_(Number_is_Prime_or_not).java
+++ Day_8_(Number_is_Prime_or_not).java
@@ -15,7 +15,7 @@ public class Day08 {
         }
         else {
             boolean isPrime=true;
-            for (int i=1;i<=n-1;i++){
+            for (int i=2;i<=n-1;i++){
                 if (n%i==0){ //n is a multiple of i (i not equal to 1 or n)
                     isPrime=false;
                 }
