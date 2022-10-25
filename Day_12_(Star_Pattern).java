40d20b48ec9b683ad333eb2bf185f1daeb9f7031
SARVESH KUMAR YADAV <105293807+sarveshy200@users.noreply.github.com>
2022-10-25 18:22:57 +0530

    Update Day_12_(Star_Pattern).java

--- Day_12_(Star_Pattern).java
+++ Day_12_(Star_Pattern).java
@@ -4,10 +4,13 @@
 public class pattern3 {
     public static void main(String[] args) {
         int n=5;
+        //outer loop for line
         for (int i = 1; i <= n; i++) {
+            //loop for space 
             for (int j = 1; j <= n - i; j++) {
                 System.out.print(" ");
             }
+            //inner loop for printing star
             for (int j = 1; j <= i; j++) {
                 System.out.print("*");
             }
