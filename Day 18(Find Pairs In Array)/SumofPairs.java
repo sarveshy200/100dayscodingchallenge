58dddb01871d9231cebb053ff5647058373ce929
SARVESH KUMAR YADAV <105293807+sarveshy200@users.noreply.github.com>
2022-10-31 21:52:50 +0530

    Add files via upload

---
+++ Day 18(Find Pairs In Array)/SumofPairs.java
@@ -0,0 +1,25 @@
+public class SumofPairs  {
+    public static void main(String[] args) {
+
+        int arr[]={1,2,3,4,5,6,7};
+        int sum=9;
+        int low=0;
+        int high=arr.length-1;
+        while (low<high){
+            if (arr[low]+arr[high]>sum){
+                high--;
+            } else if (arr[low]+arr[high]<sum) {
+                low++;
+
+            } else if ((arr[low]+arr[high])==sum) {
+                System.out.println("pair("+arr[low]+" , "+arr[high]+")");
+                low++;
+                high--;
+
+            }
+        }
+
+    }
+
+
+}
