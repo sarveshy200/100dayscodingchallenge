bed0ccfe651b50d4f3acb56f4382b11af176b7a0
SARVESH KUMAR YADAV <105293807+sarveshy200@users.noreply.github.com>
2022-11-02 20:50:51 +0530

    Add files via upload

---
+++ Day 20 (Remove Dublicate Element in Array)/RemoveDublicate.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+
+public class RemoveDublicate {
+    public static void removeDuplicateUsingSorting(int arr[]) {
+
+        //Sort an unsorted array
+        Arrays.sort(arr);
+
+        int len = arr.length;
+        int j = 0;
+
+        //Traverse an array
+        for (int i = 0; i < len - 1; i++) {
+
+            //if value present at i and i+1 index is not equal
+            if (arr[i] != arr[i + 1]) {
+                arr[j++] = arr[i];
+            }
+        }
+
+        arr[j++] = arr[len - 1];
+
+        for (int k = 0; k < j; k++) {
+            System.out.print(arr[k] + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int arr[] = { 5, 1, 2, 6, 4, 4, 5, 6, 8, 7 };
+        removeDuplicateUsingSorting(arr);
+
+    }
+}
+
