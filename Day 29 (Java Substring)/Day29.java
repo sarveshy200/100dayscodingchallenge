fe64633a98d4244e8052024673686684219f507a
SARVESH KUMAR YADAV <105293807+sarveshy200@users.noreply.github.com>
2022-11-11 23:00:37 +0530

    Add files via upload

---
+++ Day 29 (Java Substring)/Day29.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Day29 {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        //input the string from the user
+        String S = in.next();
+        int start = in.nextInt();
+        int end = in.nextInt();
+        //printing the substring
+        System.out.println(S.substring(3,7));
+    }
+}
