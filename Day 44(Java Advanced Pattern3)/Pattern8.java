e42f51214e4edcd0db3dd7d72ac193c47a30cdc7
SARVESH KUMAR YADAV <105293807+sarveshy200@users.noreply.github.com>
2022-11-26 23:05:25 +0530

    Add files via upload

---
+++ Day 44(Java Advanced Pattern3)/Pattern8.java
@@ -0,0 +1,40 @@
+public class Pattern8 {
+    public static void ButterFly(int n){
+        //1st half
+        for (int i=1;i<=n;i++){
+            //stars-->i
+            for (int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            //space-->2*(n-i)
+            for (int j=1;j<=2*(n-i);j++){
+                System.out.print(" ");
+            }
+            //stars-->i
+            for (int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        //2nd half
+        for (int i=n;i>=1;i--){
+            for (int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            //space-->2*(n-i)
+            for (int j=1;j<=2*(n-i);j++){
+                System.out.print(" ");
+            }
+            //stars-->i
+            for (int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+
+    public static void main(String[] args) {
+        ButterFly(4);
+    }
+}
