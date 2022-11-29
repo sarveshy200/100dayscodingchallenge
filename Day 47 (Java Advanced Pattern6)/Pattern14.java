341ab601e5cf12c2c1d9f83b958e04bca4fa88aa
SARVESH KUMAR YADAV <105293807+sarveshy200@users.noreply.github.com>
2022-11-29 23:47:48 +0530

    Add files via upload

---
+++ Day 47 (Java Advanced Pattern6)/Pattern14.java
@@ -0,0 +1,36 @@
+public class Pattern14 {
+    public static void Number(int n) {
+        int counter = 1;
+
+        for (int i = 1; i<=n; i++) {
+
+            for (int j = 0; j <= i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = i; j <= n; j++) {
+                System.out.print(j+" ");
+
+
+            }
+            System.out.println();
+
+
+        }
+        for (int i = n-1; i>=1; i--) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = i; j <= n; j++) {
+                System.out.print(j+" ");
+
+
+            }
+            System.out.println();
+
+
+        }
+    }
+    public static void main(String[] args) {
+        Number(6);
+    }
+}
